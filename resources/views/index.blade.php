94d82e10da749ef3fa6f56d2360a096c8d071464
marianaviro <marianaviro@hotmail.com>
2015-06-22 18:45:00 -0500

    Cambios en CSS y Parallax agregado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -58,6 +58,8 @@
 <script src="{{ asset('/js/skel.min.js') }}"></script>
 <script src="{{ asset('/js/util.js') }}"></script>
 <!--[if lte IE 8]><script src="{{ asset('/js/ie/respond.min.js') }}"></script><![endif]-->
+<script src="{{ asset('/js/imagesloaded.js') }}"></script>
+<script src="{{ asset('/js/skrollr.js') }}"></script>
 <script src="{{ asset('/js/main.js') }}"></script>
 
 </body>
